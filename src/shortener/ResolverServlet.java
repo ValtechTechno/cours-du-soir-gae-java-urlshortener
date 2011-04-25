766836d5c96005f8ae091b836ee57809aef18731
Claude Falguiere <cfalguiere@gmail.com>
2011-04-25 14:24:48 +0200

    added resolver servlet

--- src/shortener/ResolverServlet.java
+++ src/shortener/ResolverServlet.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class Demo2Servlet extends HttpServlet {
+public class ResolverServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		String shortUrl = req.getPathInfo();
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, world " + shortUrl.substring(1));
 	}
 }
